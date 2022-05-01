4c798ca04f38b3322f0066eacdfa729c7eab4e31
Vladimer96 <>
2022-05-01 04:31:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+date = input("Введите дату:")
+task = input("Введите задачу:")
+date1 = input("Введите дату:")
+task1 = input("Введите задачу:")
+date2 = input("Введите дату:")
+task2 = input("Введите задачу:")
+# print(date,task)
+# print(date1,task1)
+# print(date1,task1)
+kalendar={
+  date: task,
+  date1: task1,
+  date2: task2
+}
+# print(kalendar)
